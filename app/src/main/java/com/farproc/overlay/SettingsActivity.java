72626c719b3a67545f00850d052924491fbc2f8c
Kevin@MBP <farproc@gmail.com>
2016-05-21 09:11:49 +0800

    Move master switch to action bar.

--- app/src/main/java/com/farproc/overlay/SettingsActivity.java
+++ app/src/main/java/com/farproc/overlay/SettingsActivity.java
@@ -8,11 +8,14 @@ import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -25,14 +28,79 @@ public class SettingsActivity extends AppCompatActivity {
     public static final String PREF_KEY_KEEP_SCREEN_ON = "pref_key_keep_screen_on";
     public static final String PREF_KEY_MASTER_SWITCH = "pref_key_master_switch";
 
-    public static class PrefsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+    public static class PrefsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        private SharedPreferences mPrefs;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.settings);
+            mPrefs = getPreferenceManager().getSharedPreferences();
             setupPreferences();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            mPrefs.registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            mPrefs.unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+            getActivity().getMenuInflater().inflate(R.menu.settings_opt, menu);
+        }
+
+        @Override
+        public void onPrepareOptionsMenu (Menu menu) {
+            final Switch masterSwitch = (Switch) menu.findItem(R.id.master_switch).getActionView();
+            final boolean masterSwitchOn = mPrefs.getBoolean(PREF_KEY_MASTER_SWITCH, false);
+            masterSwitch.setChecked(masterSwitchOn);
+            masterSwitch.setText(masterSwitchOn ? R.string.action_on : R.string.action_off);
+            masterSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    ((Switch) buttonView).setText(isChecked ? R.string.action_on : R.string.action_off);
+                    if(mPrefs.getBoolean(PREF_KEY_MASTER_SWITCH, false) != isChecked) {
+                        mPrefs.edit().putBoolean(PREF_KEY_MASTER_SWITCH, isChecked).commit();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+            switch(key) {
+                case PREF_KEY_MASTER_SWITCH: {
+                    getActivity().invalidateOptionsMenu();
+                    final boolean masterSwitchOn = prefs.getBoolean(key, false);
+                    findPreference(PREF_KEY_FORCE_IMMERSIVE).setEnabled(masterSwitchOn);
+                    findPreference(PREF_KEY_FORCE_ROTATION).setEnabled(masterSwitchOn);
+                    findPreference(PREF_KEY_KEEP_SCREEN_ON).setEnabled(masterSwitchOn);
+                    findPreference(PREF_KEY_BLUE_FILTER).setEnabled(masterSwitchOn);
+                    // No break here.
+                }
+                case PREF_KEY_FORCE_IMMERSIVE:
+                case PREF_KEY_FORCE_ROTATION:
+                case PREF_KEY_BLUE_FILTER:
+                case PREF_KEY_KEEP_SCREEN_ON:
+                    if(prefs.getBoolean(PREF_KEY_MASTER_SWITCH, false) &&
+                            (prefs.getBoolean(PREF_KEY_FORCE_IMMERSIVE, false) ||
+                                prefs.getBoolean(PREF_KEY_FORCE_ROTATION, false) ||
+                                prefs.getBoolean(PREF_KEY_BLUE_FILTER, false) ||
+                                prefs.getBoolean(PREF_KEY_KEEP_SCREEN_ON, false))) {
+                        startOverlayService(getActivity());
+                    }
+                    break;
+            }
         }
 
         private class BlueFilterLevelListener implements SeekBar.OnSeekBarChangeListener, SeekBarPreference.OnShowDialogListener, SeekBarPreference.OnDialogClosedListener {
@@ -69,7 +137,7 @@ public class SettingsActivity extends AppCompatActivity {
             public void onDialogClosed(boolean positiveResult) {
                 final Activity activity = getActivity();
                 if(mService != null) {
-                    mService.setBlueFilterLevel(getPreferenceManager().getSharedPreferences().getInt(PREF_KEY_BLUE_FILTER_LEVEL, 50));
+                    mService.setBlueFilterLevel(mPrefs.getInt(PREF_KEY_BLUE_FILTER_LEVEL, 50));
                 }
                 activity.unbindService(mConn);
             }
@@ -84,23 +152,14 @@ public class SettingsActivity extends AppCompatActivity {
         private BlueFilterLevelListener mBlueFilterLevelListener = new BlueFilterLevelListener();
 
         private void setupPreferences() {
-            findPreference(PREF_KEY_MASTER_SWITCH).setOnPreferenceChangeListener(this);
-            findPreference(PREF_KEY_FORCE_IMMERSIVE).setOnPreferenceChangeListener(this);
-            findPreference(PREF_KEY_FORCE_ROTATION).setOnPreferenceChangeListener(this);
-            findPreference(PREF_KEY_BLUE_FILTER).setOnPreferenceChangeListener(this);
-
-            final SharedPreferences prefs = getPreferenceManager().getSharedPreferences();
-            if(!prefs.contains(PREF_KEY_BLUE_FILTER_LEVEL)) {
-                prefs.edit().putInt(PREF_KEY_BLUE_FILTER_LEVEL, 50).commit();
+            if(!mPrefs.contains(PREF_KEY_BLUE_FILTER_LEVEL)) {
+                mPrefs.edit().putInt(PREF_KEY_BLUE_FILTER_LEVEL, 50).commit();
             }
             final SeekBarPreference blueFilterLevel = (SeekBarPreference)findPreference(PREF_KEY_BLUE_FILTER_LEVEL);
-            blueFilterLevel.setOnPreferenceChangeListener(this);
             blueFilterLevel.setOnSeekbarChangedListener(mBlueFilterLevelListener);
             blueFilterLevel.setOnShowDialogListener(mBlueFilterLevelListener);
             blueFilterLevel.setOnDialogClosedListener(mBlueFilterLevelListener);
             //blueFilterLevel.setEnabled(prefs.getBoolean(PREF_KEY_BLUE_FILTER, false));
-
-            findPreference(PREF_KEY_KEEP_SCREEN_ON).setOnPreferenceChangeListener(this);
 
             startOverlayServiceIfNeeded(getActivity());
         }
@@ -116,38 +175,11 @@ public class SettingsActivity extends AppCompatActivity {
             }
         }
 
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            final Activity activity = getActivity();
-            final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
-            switch(preference.getKey()) {
-                case PREF_KEY_MASTER_SWITCH:
-                    if((boolean)newValue &&
-                            (prefs.getBoolean(PREF_KEY_FORCE_IMMERSIVE, false) ||
-                                prefs.getBoolean(PREF_KEY_FORCE_ROTATION, false) ||
-                                prefs.getBoolean(PREF_KEY_BLUE_FILTER, false) ||
-                                prefs.getBoolean(PREF_KEY_KEEP_SCREEN_ON, false))) {
-                            startOverlayService(activity);
-                        }
-                    break;
-                case PREF_KEY_FORCE_IMMERSIVE:
-                case PREF_KEY_FORCE_ROTATION:
-                case PREF_KEY_BLUE_FILTER:
-                case PREF_KEY_KEEP_SCREEN_ON:
-                    if((boolean)newValue && prefs.getBoolean(PREF_KEY_MASTER_SWITCH, false)) {
-                        startOverlayService(activity);
-                    }
-                    break;
-            }
-            return true;
-        }
-
         private static void startOverlayService(Context context) {
             // The service is responsible to stop itself.
             context.startService(new Intent(context, OverlayService.class));
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
